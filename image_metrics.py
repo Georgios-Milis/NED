0e74f4cf63b8ef982ffe503e6c5f8da4eec6aa1a
George Milis <el18035@mail.ntua.gr>
2023-11-08 19:00:05 +0200

    Image metrics.

---
+++ image_metrics.py
@@ -0,0 +1,114 @@
+import os
+import sys
+
+import cv2
+import numpy as np
+from pytorch_msssim import SSIM
+import torch
+
+from DECA.decalib.utils import util
+
+
+# FID, SSIM (F/M), APD (F/M)
+
+def read_masks(dir, max_n_sequences=None):
+    masks = []
+    fnames = sorted(os.walk(dir))
+    for fname in sorted(fnames):
+        paths = []
+        root = fname[0]
+        for f in sorted(fname[2]):
+            paths.append(os.path.join(root, f))
+        if len(paths) > 0:
+            masks.append(paths)
+    if max_n_sequences is not None:
+        masks = masks[:max_n_sequences]
+    return masks
+
+
+def read_image_batch(image_dir, H=256, W=256):
+    image_files = [f for f in sorted(os.listdir(image_dir)) if f.endswith('.png')]
+    image_data = np.empty((len(image_files), 3, H, W))
+
+    for i, image_file in enumerate(image_files):
+        image = cv2.imread(os.path.join(image_dir, image_file))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_data[i] = image.transpose(2, 0, 1)
+
+    return torch.from_numpy(image_data).cuda()
+
+
+def transform_points(points, mat):
+    points = np.expand_dims(points, axis=1)
+    points = cv2.transform(points, mat, points.shape)
+    points = np.squeeze(points)
+    return points
+
+
+def get_mats_paths(dir):
+    mats_files = []
+    assert os.path.isdir(dir), '%s is not a valid directory' % dir
+    for root, _, fnames in sorted(os.walk(dir)):
+        for fname in sorted(fnames):
+            if fname.endswith('txt'):
+                path = os.path.join(root, fname)
+                mats_files.append(path)
+    return mats_files
+
+
+def APD(img1, img2):
+    return torch.nn.functional.mse_loss(img1, img2).item()
+
+
+X_dir = sys.argv[1]
+Y_dir = sys.argv[2]
+
+X = read_image_batch(X_dir)
+Y = read_image_batch(Y_dir)
+
+batch_size = min(X.shape[0], Y.shape[0])
+X = X[:batch_size]
+Y = Y[:batch_size]
+
+# Reuse the Gaussian kernel with SSIM & MS_SSIM 
+ssim_module = SSIM().cuda()
+# ms_ssim_module = MS_SSIM()
+
+ssim_val = ssim_module(X, Y).item()
+print(f"SSIM: {100 * ssim_val:.2f}%")
+
+
+
+face_attr_mask = util.load_local_mask(image_size=256, mode='bbx')
+m = face_attr_mask[3]
+
+# image = X[0]
+# import matplotlib.pyplot as plt
+
+# image = image.permute(1, 2, 0) / 255
+# m is a bounding box: l r t b
+# 
+
+mats_dir = os.path.join(X_dir.replace('images', 'align_transforms'))
+mat_paths = sorted(get_mats_paths(mats_dir))
+
+X_mouths = X
+Y_mouths = Y
+
+X_mouths = X[..., m[2]:m[3], m[0]:m[1]]
+Y_mouths = Y[..., m[2]:m[3], m[0]:m[1]]
+
+mapd = APD(X_mouths, Y_mouths)
+print(f"MAPD: {mapd}")
+
+
+# import matplotlib.pyplot as plt
+# plt.figure()
+# plt.imshow(Y_mouths[4].cpu().permute(1, 2, 0) / 255)
+# plt.savefig('mouth.png')
+# raise
+
+
+masks = 
+
+X_faces = [x*mask]
--- metrics.sh
+++ metrics.sh
@@ -2,33 +2,26 @@
 set -e
 
 
-subject="21M"
-test="1_9_pred"
+folder=$1
+subject=$2
 
-# python preprocessing/detect.py --celeb ../SadTalker/results
-
-
-for i in $(seq 10)
+for file in $(ls $folder/*.mp4)
 do
-    test=1_${i}_pred
-
-    echo $subject, $test
-
-    python save_frames.py ../SadTalker/results/$test.mp4 ../SadTalker/results/$test
-
-    # X_dir=test_examples/$subject/images
-    # Y_dir=test_examples/$subject/$test/images
+    # mkdir -p tmp/`basename $file`
+    # /home/gmil/miniconda3/envs/NED/bin/python save_frames.py $file tmp/`basename $file`
 
     X_dir=test_examples/$subject/images
-    Y_dir=../SadTalker/results/$test
+    Y_dir=tmp/`basename $file`
 
     # # Compress original actor images
     # python -m pytorch_fid \
     #     --save-stats train_examples/${subject}_3/images/000000 $subject-FID.npz
 
     # FID
-    python -m pytorch_fid $subject-FID.npz $Y_dir --device cuda 
+    # python -m pytorch_fid $subject-FID.npz $Y_dir --device cuda
+    # /home/gmil/miniconda3/envs/NED/bin/python \
+    #     -m pytorch_fid $X_dir $Y_dir --device cuda
 
-    # SSIM
-    python metrics.py $X_dir $Y_dir
+    # SSIM, APD
+    /home/gmil/miniconda3/envs/NED/bin/python image_metrics.py $X_dir $Y_dir
 done
